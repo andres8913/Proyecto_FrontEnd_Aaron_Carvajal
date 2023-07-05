47c71184a54a7e9ce659ecb11fab73ac17b2d936
Your Name <you@example.com>
2023-07-04 22:13:24 -0500

    Validacion Formulario BackEnd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('hola js');
+console.log("hola js")
 
 function saveCustomer(){
 
@@ -11,6 +11,21 @@ function saveCustomer(){
     let profesionCustomer = document.getElementById("profesion");
     let rolCustomer = document.getElementById("rol");
 
+    if (
+        numeroIdentificacionCustomer.value === '' ||
+        nombresCustomer.value === '' ||
+        apellidosCustomer.value === '' ||
+        tipoIdentificacionCustomer.value === '' ||
+        telefonoCustomer.value === '' ||
+        emailCustomer.value === '' ||
+        profesionCustomer.value === '' ||
+        rolCustomer.value === ''
+    ) {
+        alert("Por Favor Diligenciar Todos Los Campos");
+        return false;
+    }
+
+
     let customer = {
         numeroIdentificacion: numeroIdentificacionCustomer.value,
         nombres: nombresCustomer.value,
@@ -22,79 +37,37 @@ function saveCustomer(){
         rol: rolCustomer.value
     }
 
-    console.log(customer);
+console.log(customer);
 
     let url = "http://localhost:8000/api/customer";
-
-    fetch(url,{
+    let params = {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
+        headers:{
+
+            "Content-Type": "application/json" 
         },
+
         body: JSON.stringify(customer),
-    }).then(response =>{
+
+    }
+
+    fetch(url,params).then((response) =>{
 
         console.log(response);
+        console.log(response.json());
+
+        if(response.status == 200){
+
+            alert("Informacion Registrada Correctamente");
+            document.getElementById("contactForm").reset();
+
+        }else{
+
+            alert("El Usuario Ya Esta Registrado, Pro Favor Verifique La Informacion");
+
+        }
     
-    })
+    });
 
-    alert("Cliente Creado")
-
-    return true;
+return true;
 }
-
-//Guardar Datos en IndexedDB
-// function processform(event) {
-//     event.preventDefault();
-
-//     var names = document.getElementById("Names").value;
-//     var lastname = document.getElementById("lastName").value;
-//     var lLine = document.getElementById("landLine").value;
-//     var cPhone = document.getElementById("cellPhone").value;
-//     var oAddres = document.getElementById("officeAddress").value;
-//     var Email = document.getElementById("email").value;
-
-//     if (names === '' || lastname === '' || lLine === '' || cPhone === '' || oAddres === '' || Email === '') {
-//         alert("Por favor, complete todos los campos del formulario.");
-//         return;
-//     }
-
-//     var user = {
-//         Names: names,
-//         lastName: lastname,
-//         landLine: lLine,
-//         cellPhone: cPhone,
-//         officeAddress: oAddres,
-//         email: Email,
-//     }
-
-   
-//     console.log(user);
-//     var dataBase = window.indexedDB.open("bdFront", 5);
-
-//     dataBase.onsuccess = (event) => {
-//         console.log("DB creada");
-//         let db = dataBase.result;
-//         console.log(db);
-//         var instanceTransaction = db.transaction("users", "readwrite").objectStore("users");
-//         var saveData = instanceTransaction.add(user);
-//         alert("Su Información Se Ha Enviado Correctamente, Gracias Por Elegirme");
-//         form.reset();
-//     };
-
-//     dataBase.onupgradeneeded = (event) => {
-//         let db = event.target.result;
-//         var createObjectData = db.createObjectStore("users", { keyPath: "Names" });
-//     };
-
-
-//  }
-// var form = document.getElementById("contactForm");
-// form.addEventListener("submit", processform);
-
-
-
-
-
-
-
